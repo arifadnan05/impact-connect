06dfe301ec1c20fadcbc8975443c35051aa8e624
arifadnan05 <arifahamedadnan@gmail.com>
2024-10-14 11:50:29 +0600

    banner updated

--- src/Components/Pages/Home/Banner/Carousel.jsx
+++ src/Components/Pages/Home/Banner/Carousel.jsx
@@ -29,9 +29,9 @@ export default function Carousel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><Slide></Slide></SwiperSlide>
-        <SwiperSlide><Slide></Slide></SwiperSlide>
-        <SwiperSlide><Slide></Slide></SwiperSlide>
+        <SwiperSlide><Slide img={'https://avenica.com/wp-content/uploads/2021/12/volunteer-blog-photo-scaled.jpg'} header={'Find Volunteer From Hare'}></Slide></SwiperSlide>
+        <SwiperSlide><Slide img={'https://static.jobscan.co/blog/uploads/Volunteers-working-together-1.jpg'} header={'Find Job From Hare'}></Slide></SwiperSlide>
+        <SwiperSlide><Slide img={'https://t4.ftcdn.net/jpg/06/50/38/07/360_F_650380719_otsngBrQUanLDCeW5cUNIg8uS9s9HxBV.jpg'} header={'Make Happy Everyone'}></Slide></SwiperSlide>
       
       </Swiper>
     </>
--- src/Components/Pages/Home/Banner/Slide.jsx
+++ src/Components/Pages/Home/Banner/Slide.jsx
@@ -1,16 +1,17 @@
 
-const Slide = () => {
+// eslint-disable-next-line react/prop-types
+const Slide = ({img, header}) => {
     return (
         <div
             className='w-full bg-center bg-cover h-[500px]'
             style={{
-                backgroundImage: `url("https://avenica.com/wp-content/uploads/2021/12/volunteer-blog-photo-scaled.jpg")`,
+                backgroundImage: `url("${img}")`,
             }}
         >
             <div className='flex items-center justify-center w-full h-full bg-gray-900/70'>
                 <div className='text-center'>
                     <h1 className='text-3xl font-semibold text-white lg:text-4xl'>
-                        Build your new <span className='text-blue-400'>Saas</span> Project
+                        {header}
                     </h1>
                     <br />
                     <button className='w-full px-5 py-4 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-gray-600 rounded-md lg:w-auto hover:bg-gray-500 focus:outline-none focus:bg-gray-500'>
