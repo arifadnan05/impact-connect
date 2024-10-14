4ef3f7da9ec45c92f4b9faf0e0ed85c7de881103
arifadnan05 <arifahamedadnan@gmail.com>
2024-10-14 11:02:08 +0600

    Home screen loader added

--- src/Components/Layout/Root.jsx
+++ src/Components/Layout/Root.jsx
@@ -3,19 +3,47 @@ import Navbar from '../Pages/Shared/Navbar/Navbar'
 import Footer from '../Pages/Shared/Footer/Footer'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import { PulseLoader } from 'react-spinners';
 
 const Root = () => {
+    const [loading, SetLoading] = useState(true);
+
+    useEffect(() => {
+        SetLoading(true)
+
+        setTimeout(() => {
+            SetLoading(false)
+        }
+            , 1900)
+    }, [])
+
     return (
-        <div>
-            <div className='px-11'>
-                <Navbar></Navbar>
-                <ToastContainer />
-            </div>
-            <div className='container mx-auto px-6 lg:px-24'>
-                <Outlet></Outlet>
-            </div>
-            <Footer></Footer>
-        </div>
+        <>
+            {
+                loading ? (
+                    <div className='flex justify-center h-[100vh] items-center'><PulseLoader
+                        color={'#9067C6'}
+                        loading={true}
+                        size={30}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    /></div>
+                ) : (
+                    <div>
+                        <div className='px-11'>
+                            <Navbar></Navbar>
+                            <ToastContainer />
+                        </div>
+                        <div className='container mx-auto px-6 lg:px-24'>
+                            <Outlet></Outlet>
+                        </div>
+                        <Footer></Footer>
+                    </div>
+                )
+            }
+        </>
+
     )
 }
 
